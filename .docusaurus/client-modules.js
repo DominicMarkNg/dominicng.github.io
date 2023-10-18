9b50bd6281f68a325d778d74fc6df48ea885e854
DominicMarkNg <dominicmarkng1@gmail.com>
2023-10-18 13:15:55 +0100

    Website Creation

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/dominicng/Documents/GitHub/dominicng.github.io/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/dominicng/Documents/GitHub/dominicng.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/dominicng/Documents/GitHub/dominicng.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/dominicng/Documents/GitHub/dominicng.github.io/src/css/custom.css'),
+];
